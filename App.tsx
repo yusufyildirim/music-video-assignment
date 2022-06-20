a6528cdb0352bd967d5025f6a891259d72c4e60f
Yusuf YILDIRIM <yusuf@tinythin.gs>
2022-06-20 18:54:20 +0300

    chore: add react-navigation and fix the type resolution issue

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
+import * as React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+
+const Stack = createNativeStackNavigator()
+
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
