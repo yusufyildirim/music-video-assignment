1d46d5b0ac62e77d3c2fce6dd2845277e450e10b
Yusuf YILDIRIM <yusuf@tinythin.gs>
2022-06-21 00:39:18 +0300

    chore: migrate to expo-dev-client for better control

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'expo-dev-client'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Text } from '@xi/design-system.text'
