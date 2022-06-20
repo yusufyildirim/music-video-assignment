4997b4e392ef59477d57e56de82ca9b2db96e95a
Yusuf YILDIRIM <yusuf@tinythin.gs>
2022-06-20 19:41:11 +0300

    chore: add jest and react-testing-library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'jest-expo',
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+}
