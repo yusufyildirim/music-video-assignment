d31f55bc0e52fa0b9ea44ff49adb980f55974939
Yusuf YILDIRIM <yusuf@tinythin.gs>
2022-06-21 22:59:52 +0300

    chore: make linter rules stricter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
   extends: 'universe/native',
+  plugins: ['react-native'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'error',
+    'react-native/no-color-literals': 'error',
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-single-element-style-arrays': 'error',
+    'react-native/no-unused-styles': 'error',
+    'react-native/sort-styles': [
+      'error',
+      'asc',
+      { ignoreClassNames: false, ignoreStyleProperties: true },
+    ],
+  },
 }
