227b4defca1dd34998642e4f2939891f8989a90f
Yusuf YILDIRIM <yusuf@tinythin.gs>
2022-06-26 22:15:46 +0300

    feat: handle no search result cases

---
+++ packages/search-and-discovery/search-result-modal/components/NoSearchResult.tsx
@@ -0,0 +1,33 @@
+import { Text } from '@xi/design-system.text'
+import { fontSize, spacing } from '@xi/design-system.theme'
+import { StyleSheet, View } from 'react-native'
+
+import { useSearchFilters } from '../context'
+
+export function NoSearchResult() {
+  const { filters } = useSearchFilters()
+
+  return (
+    <View style={styles.container}>
+      <Text fontSize={fontSize.body1} style={styles.text}>
+        Couldn't find any result
+      </Text>
+      {filters.text && <Text style={styles.text}>for"{filters.text}"</Text>}
+      <Text style={styles.text} color="suppressed">
+        Try searching again with different keyword and/or filter combination
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    paddingVertical: spacing.s4,
+  },
+})
--- packages/search-and-discovery/search-result-modal/components/SearchResultsContentPainter.tsx
+++ packages/search-and-discovery/search-result-modal/components/SearchResultsContentPainter.tsx
@@ -1,7 +1,9 @@
 import { ContentPainter } from '@xi/browsing-experience.content'
+import React from 'react'
 
 import { useSearchFilters } from '../context'
 import { useSearchResultContentQuery } from '../hooks'
+import { NoSearchResult } from './NoSearchResult'
 
 export function SearchResultsContentPainter() {
   const { filters } = useSearchFilters()
@@ -9,6 +11,9 @@ export function SearchResultsContentPainter() {
   const { isLoading, data } = useSearchResultContentQuery(filters)
 
   if (isLoading) return null
+
+  // Means that there's no match
+  if (data?.length === 0) return <NoSearchResult />
 
   return <ContentPainter collections={data || []} keyboardDismissMode="on-drag" />
 }
